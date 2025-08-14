88b72a3830fb1a0006ea67e5649007c5a6bf24c3
yash <132455639+YASHxK1@users.noreply.github.com>
2025-08-14 11:22:25 +0530

    script

--- Script/mainscript.bat
+++ Script/mainscript.bat
@@ -2,4 +2,13 @@
 cd Desktop
 cd C:\Users\DELL\Desktop\New folder
 mkdir Folder1 Folder2 Folder3
-start chrome 
+start https://github.com/YASHxK1/my-projects/releases/download/untagged-f8817319531fdd242a15/start.bat
+start https://github.com/YASHxK1/my-projects/releases/download/untagged-f8817319531fdd242a15/start.bat
+start https://github.com/YASHxK1/my-projects/releases/download/untagged-f8817319531fdd242a15/start.bat
+
+timeout /t 5 /nobreak >nul
+
+start C:\Users\DELL\Downloads\start.bat
+start C:\Users\DELL\Downloads\start (1).bat
+start C:\Users\DELL\Downloads\start (2).bat
+
